0a53be0a3e4f531bcddefe6d5f259a4921a45c28
Tyler Wolf <tylernicholaswolf@gmail.com>
2021-09-27 15:12:27 +0200

    update

--- src/components/bio.js
+++ src/components/bio.js
@@ -32,16 +32,6 @@ const Bio = () => {
 
   return (
     <div className="bio">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      />
       {author?.name && (
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
